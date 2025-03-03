5bfff02d6c1078a97b82fac0fffb2553af5768ba
Iman <bajalaniman@gmail.com>
2025-03-03 19:46:01 +0100

    first

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.get("/messages", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}.`);
 });
